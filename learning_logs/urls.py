4f83fea779a7725f217dac1fbf797792e47ed2cc
Vince <vincentjayden49@gmail.com>
2020-05-23 00:34:12 -0700

    First commit

---
+++ learning_logs/urls.py
@@ -0,0 +1,14 @@
+#url oatterns for learning logs
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+    #Homepage
+    path('', views.index, name = 'index'),
+    path('topics/', views.topics, name = 'topics'),
+    #Details page
+    path('topics/<int:topic_id>/', views.topic, name = 'topic')
+]
