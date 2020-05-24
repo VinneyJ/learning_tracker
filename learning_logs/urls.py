ea98b854ebf79c51e86bb22731583ad8e68e505f
Vince <vincentjayden49@gmail.com>
2020-05-24 00:10:08 -0700

    Added Topics & Entries

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,5 +10,10 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     path('topics/', views.topics, name = 'topics'),
     #Details page
-    path('topics/<int:topic_id>/', views.topic, name = 'topic')
+    path('topics/<int:topic_id>/', views.topic, name = 'topic'),
+    #To new topic page
+    path('new_topic/', views.new_topic, name = 'new_topic'),
+    path('new_topic/<int:topic_id>', views.new_entry, name = 'new_entry'),
+    #Edit entry
+    path('edit_entry/<int:entry_id>', views.edit_entry, name='edit_entry')
 ]
